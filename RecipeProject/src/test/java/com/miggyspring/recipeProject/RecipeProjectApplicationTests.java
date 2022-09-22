29105a03a410a71e5bf99bca47a06be1237e4ece
MIGGY-TERA <mgmravarra.trsys@gmail.com>
2022-09-22 16:22:22 +0800

    Initial Commit

---
+++ RecipeProject/src/test/java/com/miggyspring/recipeProject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miggyspring.recipeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
