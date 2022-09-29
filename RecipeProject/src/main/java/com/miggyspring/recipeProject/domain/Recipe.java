a7335601077f3ce5e8e7a69b1722a69a642d3b96
markravarra <markgideon.ravarra@gmail.com>
2022-09-29 10:13:29 +0800

    Add Ingredient Data Model and Establish relationship with recipe

--- RecipeProject/src/main/java/com/miggyspring/recipeProject/domain/Recipe.java
+++ RecipeProject/src/main/java/com/miggyspring/recipeProject/domain/Recipe.java
@@ -1,11 +1,14 @@
 package com.miggyspring.recipeProject.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -24,7 +27,10 @@ public class Recipe {
 	private String directions;
 	// TODO: add
 	// private Difficulty difficulty;
-	
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+
 	@Lob
 	private Byte[] image;
 
@@ -93,6 +99,14 @@ public class Recipe {
 
 	public void setDirections(String directions) {
 		this.directions = directions;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 	public Byte[] getImage() {
